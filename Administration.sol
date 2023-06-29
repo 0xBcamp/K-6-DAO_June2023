c73627801882f156ab7579b9029963a2ceec3672
Jakylan <101082752+Jakylan1@users.noreply.github.com>
2023-06-29 13:36:01 -0400

    Update Administration.sol

--- Administration.sol
+++ Administration.sol
@@ -13,39 +13,34 @@ contract Administration {
         emit MainContractSet(mainContract); // Emit the MainContractSet event
     }
 
-    function accessStudentProfile(
-        address _studentAddress
-    ) external view returns (string memory) {
+    function accessStudentProfile(address _studentAddress) external view returns (string memory) {
         require(mainContract != address(0), "Main contract is not set."); // Check if the main contract is set
         return MainContract(mainContract).getStudentProfile(_studentAddress); // Call the getStudentProfile function of the main contract
     }
 
-    function accessTutorProfile(
-        address _tutorAddress
-    ) external view returns (string memory) {
+    function accessTutorProfile(address _tutorAddress) external view returns (string memory) {
         require(mainContract != address(0), "Main contract is not set."); // Check if the main contract is set
         return MainContract(mainContract).getTutorProfile(_tutorAddress); // Call the getTutorProfile function of the main contract
     }
 
     function editDAOCode(uint256 _parameters) external {
         require(mainContract != address(0), "Main contract is not set."); // Check if the main contract is set
-         MainContract(mainContract).editDAOCode(_parameters); // Call the editDAOCode function of the main contract
+        MainContract(mainContract).editDAOCode(_parameters); // Call the editDAOCode function of the main contract
     }
 
-function getProfile(address _userAddress) external view returns (string[] memory) {
-    require(mainContract != address(0), "Main contract is not set."); // Check if the main contract is set
+    function getProfile(address _userAddress) external view returns (string[] memory) {
+        require(mainContract != address(0), "Main contract is not set."); // Check if the main contract is set
 
-    string memory studentProfile = MainContract(mainContract).getStudentProfile(_userAddress);
-    if (bytes(studentProfile).length == 0) {
-        string memory tutorProfile = MainContract(mainContract).getTutorProfile(_userAddress);
+        string memory studentProfile = MainContract(mainContract).getStudentProfile(_userAddress);
+        if (bytes(studentProfile).length == 0) {
+            string memory tutorProfile = MainContract(mainContract).getTutorProfile(_userAddress);
+            string[] memory profiles = new string[](1);
+            profiles[0] = tutorProfile;
+            return profiles; // Return an array containing the tutor profile
+        }
+
         string[] memory profiles = new string[](1);
-        profiles[0] = tutorProfile;
-        return profiles; // Return an array containing the tutor profile
+        profiles[0] = studentProfile;
+        return profiles; // Return an array containing the student profile
     }
-
-    string[] memory profiles = new string[](1);
-    profiles[0] = studentProfile;
-    return profiles; // Return an array containing the student profile
-}
-
 }
